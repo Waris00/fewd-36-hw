5bfa8039a7950731c2ca4e0d2279f77f9cc8ff97
Waris00 <Warace.a.p@gmail.com>
2018-02-08 00:45:41 -0500

    update
    update

---
+++ week 4/07_intro_programming/color_scheme_switcher/starter_code/js/index.js
@@ -0,0 +1,9 @@
+
+document.getElementById("grayButton").onclick = grayClick;
+
+function grayClick() {
+	
+	document.body.style.backgroundColor = "red";
+	document.body.style.color = "white";
+
+}
---
+++ week 4/07_intro_programming/traffic_light/starter_code/js/index.js
@@ -0,0 +1,31 @@
+document.getElementById('stopButton').onclick = illuminateRed;
+
+function illuminateRed() {
+  clearLights();
+  document.getElementById('stopLight').style.backgroundColor = "red";
+}
+
+
+document.getElementById('slowButton').onclick = illuminateYellow;
+
+function illuminateYellow() {
+	clearLights();
+	document.getElementById('slowLight').style.backgroundColor = "yellow";
+}
+
+
+document.getElementById('goButton').onclick = illuminateGreen;
+
+function illuminateGreen() {
+	clearLights();
+	document.getElementById('goLight').style.backgroundColor = "green";
+}
+
+
+function clearLights() {
+  document.getElementById('stopLight').style.backgroundColor = "black";
+  document.getElementById('slowLight').style.backgroundColor = "black";
+  document.getElementById('goLight').style.backgroundColor = "black";
+}
+
+
